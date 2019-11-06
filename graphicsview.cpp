d568be838071b07b4097662ab809f705235d7db1
AlessioProcelli <51055269+AlessioProcelli@users.noreply.github.com>
2019-11-06 18:39:54 +0100

    Update graphicsview.cpp

--- graphicsview.cpp
+++ graphicsview.cpp
@@ -15,8 +15,10 @@ void GraphicsView::mousePressEvent(QMouseEvent *event)
 {
     emit beginDrawing(event->x(), event->y());
 }
+
 void GraphicsView::mouseMoveEvent(QMouseEvent *event)
 {
     emit drawing(event->x(), event->y());
 }
+
 void GraphicsView::mouseReleaseEvent(QMouseEvent *event) {}
